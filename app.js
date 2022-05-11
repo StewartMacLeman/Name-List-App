8cc2e05a2cdc15fa1dd9f14053ac91c0dc98148c
Stewart MacLeman <stewart_macleman@hotmail.com>
2022-05-11 09:33:58 +0100

    Typo amended.

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/', nameRoutes);
 
-app.listen(3000, () => console.log("The app is running on port 3000."));
+// app.listen(3000, () => console.log("The app is running on port 3000."));
+app.listen(5000, () => console.log("The app is running on port 5000."));
